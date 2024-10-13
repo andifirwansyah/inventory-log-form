21fb506283307e1ad4e491f528cc9c01b52be520
andifirwansyah <andifirwansyah250@gmail.com>
2024-10-13 22:26:09 +0700

    back navigation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,37 +4,37 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@vite-pwa/nuxt', '@nuxtjs/supabase', '@pinia/nuxt'],
   plugins: ['~/plugins/auth.js'],
-  // pwa: {
-  //   manifest: {
-  //     name: 'Inventory Log Form',
-  //     short_name: 'ILF',
-  //     description: 'Inventory Log Form',
-  //     theme_color: '#22325D',
-  //     background_color: '#22325D',
-  //     icons: [
-  //       {
-  //         src: '/icon.png',
-  //         sizes: '192x192',
-  //         type: 'image/png',
-  //       },
-  //       {
-  //         src: '/icon-large.png',
-  //         sizes: '512x512',
-  //         type: 'image/png',
-  //       },
-  //     ],
-  //   },
-  //   workbox: {
-  //     // globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,svg,woff2}'],
-  //     navigateFallback: '/'
-  //   },
-  //   devOptions: {
-  //     enabled: true,
-  //   },
-  //   client: {
-  //     installPrompt: true
-  //   }
-  // },
+  pwa: {
+    manifest: {
+      name: 'Inventory Log Form',
+      short_name: 'ILF',
+      description: 'Inventory Log Form',
+      theme_color: '#22325D',
+      background_color: '#22325D',
+      icons: [
+        {
+          src: '/icon.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icon-large.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    workbox: {
+      // globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,svg,woff2}'],
+      navigateFallback: '/'
+    },
+    devOptions: {
+      enabled: true,
+    },
+    client: {
+      installPrompt: true
+    }
+  },
   supabase: {
     redirect: false,
   },
